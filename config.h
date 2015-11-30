97c41c974714c69066d3ad93a348aa52e897b64e
Lin Han <linhan@linhandeAir.lan>
2015-11-30 21:41:56 +0800

    Added one more tag.

--- config.h
+++ config.h
@@ -17,7 +17,7 @@ static const Bool topbar            = True;     /* False means bottom bar */
 
 /* tagging */
 //static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-static const char *tags[] = { "赵", "钱", "孙", "李", "周", "吴", "郑", "王" };
+static const char *tags[] = { "赵", "钱", "孙", "李", "周", "吴", "郑", "王", "冯" };
 
 static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
