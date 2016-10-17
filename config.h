f20a446baa8875f76ae370123362226c6b28f545
lanhin <lanhin1@gmail.com>
2016-10-17 19:47:29 +0800

    10 tags, firefox rules and startup shortcuts.

--- config.h
+++ config.h
@@ -17,12 +17,12 @@ static const Bool topbar            = True;     /* False means bottom bar */
 
 /* tagging */
 //static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-static const char *tags[] = { "赵", "钱", "孙", "李", "周", "吴", "郑", "王", "冯" };
+static const char *tags[] = { "青", "山", "遮", "不", "住", "毕", "竟", "东", "流", "去"};
 
 static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       False,       -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 2,       True,       -1 },
 };
 
 /* layout(s) */
@@ -59,6 +59,7 @@ static const char *mutecmd[]    = { "mixer", "vol", "0", NULL };
 static const char *lscreen[]    = { "xdotool", "mousemove", "200", "500", "click", "1", NULL };//显示屏切换，需安装 xdotool
 static const char *rscreen[]    = { "xdotool", "mousemove", "2000", "500", "click", "1", NULL };
 static const char *lockscr[] = { "lock.sh", NULL};
+static const char *startffox[]  = {"firefox", NULL};
 
 
 #include<X11/XF86keysym.h>
@@ -83,7 +84,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+	//{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },//显示屏切换，这个可能比 MODKEY+XK_Left/XK+Right 更好用
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
@@ -100,6 +101,7 @@ static Key keys[] = {
 	//{ 0,                     XF86XK_Back, focusstack,          {.i = -1 } },
 	{ MODKEY,                    XK_Left,      spawn,          {.v = lscreen } },//显示屏切换
 	{ MODKEY,                   XK_Right,      spawn,          {.v = rscreen } },
+	{ MODKEY,                       XK_o,      spawn,          {.v = startffox } },//firefox
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -109,6 +111,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
+	TAGKEYS(                        XK_0,                      9)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 	{ MODKEY,			XK_z,	   spawn,	   {.v = lockscr } },
 };
